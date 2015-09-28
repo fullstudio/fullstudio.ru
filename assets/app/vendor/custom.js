93dbbef42f637252495dcc05206a0c66782bcfcf
alexmil <milya89@gmail.com>
2015-09-28 22:49:51 +0300

    logo fix

--- assets/app/vendor/custom.js
+++ assets/app/vendor/custom.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 // makes sure the whole site is loaded
 jQuery(window).load(function() {
         // will first fade out the loading animation
-	jQuery(".status").fadeOut();
+	// jQuery(".status").fadeOut();
         // will fade out the whole DIV that covers the website.
-	jQuery(".preloader").delay(1000).fadeOut("slow");
+	jQuery(".preloader").delay(5000).fadeOut("slow");
 })
 
 /* =================================
